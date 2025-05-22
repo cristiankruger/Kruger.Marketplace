4edc3f13911439433baa25b8a9bf886e82f8a71c
Cristian Kruger <cristian.kruger@live.com>
2025-05-22 17:01:09 -0300

    fix: core

--- src/Kruger.Marketplace.Business/Models/Settings/ArquivoSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kruger.Marketplace.Business.Models.Settings
-{
-    public class ArquivoSettings
-    {
-        public const string ConfigName = "ArquivoSettings";
-        public string BasePath { get; set; }
-        public string DefaultImage { get; set; }
-    }
-}
--- src/Kruger.Marketplace.Business/Notificacoes/Notificacao.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Kruger.Marketplace.Business.Notificacoes
-{
-    public class Notificacao(string mensagem)
-    {
-        public string Mensagem { get; } = mensagem;
-    }
-}
